fcc55c919a9a7006c942447b3825198eb7453a16
jey1709 <jey1709@gmail.com>
2016-05-28 14:58:03 +0200

    bla

--- lib/src/KistenschiebenView.dart
+++ lib/src/KistenschiebenView.dart
@@ -2,11 +2,13 @@ import 'dart:html';
 
 class KistenschiebenView {
 
-  String crate = "<img src=\"../web/pictures/crate.png\" height = \"80px\" weight=\"100px\" backgroundcolor=\"blue\">";
-  String player = "<img src=\"../web/pictures/player.png\" height = \"80px\" weight=\"100px\" backgroundcolor=\"blue\">";
+  String crate = "<img src=\"../web/pictures/crate.png\">";
+  String player = "<img src=\"../web/pictures/player.png\">";
 
   KistenschiebenView() {
     print("running view...");
+
+
   }
 
   void levelrequest() {
@@ -58,6 +60,17 @@ class KistenschiebenView {
 
 
     querySelector("level").innerHtml = level;
+    querySelectorAll("td").style.height="70px";
+    querySelectorAll("td").style.width="70px";
+    querySelectorAll(".target").style.height="70px";
+    querySelectorAll(".target").style.width="70px";
+    querySelectorAll(".wall").style.height="70px";
+    querySelectorAll(".wall").style.width="70px";
+    querySelectorAll("img").style.height="70px";
+    querySelectorAll("img").style.width="70px";
+
+
+
   }
 
   void updateView(String playerPosition_old, List<String>cratePosition_old,
@@ -65,9 +78,80 @@ class KistenschiebenView {
     querySelector(playerPosition_old).innerHtml = "";
     querySelector(playerPosition_new).innerHtml = player;
 
+
     querySelectorAll("td").remove(crate);
-    querySelector(cratePosition_new.removeLast()).innerHtml = crate;
-    querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+
+    switch(cratePosition_new.length){
+      case 1: querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 2: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 3: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 4: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 5: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 6: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 7: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 8: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 9: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+      case 10: querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;
+              querySelector(cratePosition_new.removeLast()).innerHtml = crate;break;
+
+    }
+
+
+
+
   }
 
 
