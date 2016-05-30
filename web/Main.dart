2f0dbf11d3f55419c5e8ac36f0b7ccf6bb965d94
jey1709 <jey1709@gmail.com>
2016-05-30 09:28:29 +0200

    änderung 30,05

--- web/Main.dart
+++ web/Main.dart
@@ -1,13 +1,26 @@
 import 'dart:html';
 import '../lib/src/KistenschiebenController.dart';
 
-  main() {
-    KistenschiebenController control = new KistenschiebenController();
-    //control.gamekey.registerUser("Trump", "12345");
-    //control.gamekey.deleteUser("1b690dfa-3c80-4960-ba18-6d2823530635", "1234");
-    //print(await control.gamekey.changeUserName("dummy", "1234", "BaNane"));
-    //print(await control.gamekey.changeUserPassword("BaNane", "1234", "123456seven"));
-    querySelector("button").onMouseDown.listen((MouseEvent e) {
-      control.newGame();
-    });
-  }
+
+
+
+void main() {
+  KistenschiebenController control = new KistenschiebenController();
+    //control.registerUser("Peter Enis", "VeraAgina");
+
+  querySelector("#b1_3").onMouseDown.listen((MouseEvent e) {
+
+     control.newGame();
+  });
+
+
+  querySelector("b1_1").onMouseDown.listen((MouseEvent e)async{
+
+    //await control.registerUser("name","passwort");
+
+
+  });
+
+
+
+}
